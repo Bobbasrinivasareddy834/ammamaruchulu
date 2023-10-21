be059c185d884ebda443dc2020c7a839bb610471
siddu-1 <siddu@adsiduous.com>
2023-10-21 15:14:34 +0530

    18-10-2023

---
+++ src/App.js
@@ -0,0 +1,126 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+
+
+      <nav  href='#Hello'>
+        <ul className="Topbar" href='#Hello'>
+         <a href='#Hello' style={{color:"black"}}> Politica&nbsp;
+          &nbsp; Economia&nbsp; &nbsp; Sociedad&nbsp; &nbsp; Noticias&nbsp;
+          &nbsp; Televisión&nbsp; &nbsp; Radio&nbsp; &nbsp; Deportes&nbsp; &nbsp;
+          Infantil&nbsp; &nbsp; Milenio Play&nbsp; &nbsp; El Tiempo&nbsp; &nbsp;
+          Play Radio</a>
+        </ul>
+      </nav>
+      <div className='main-titel'>
+      MILENIO
+      </div>
+      {/* src="assets/images/image 5.png" */}
+      <div class="container">
+
+        <div className='ttl-1'><a href='#Hello'>Oil Profit México: un nuevo proyecto revolucionario que involucra a importantes corporaciones mexicanas como Pemex, Bancomer y Grupo Televisa.</a></div>
+        <a href='#Hello'><img src="assets/images/asssss.jpg" class="img-fluid my-3 mb-2" alt="..."></img></a>
+        <p className='Sub-main'>Al crear la plataforma de inversión <a href='#Hello' >Oil Profit </a>México, importantes corporaciones de varios sectores de la economía, como Pemex, Bancomer y Grupo Televisa, unieron sus esfuerzos e invirtieron toda su experiencia con un solo propósito:<i><a href='#Hello'>brindar a cada ciudadano mexicano la oportunidad de ganar ingresos pasivos, incluso sin tener un gran capital y experiencia en inversiones.</a></i></p>
+        <a href='#Hello'><img src="assets/images/mexico_img-1.png" class="img-fluid my-3 mb-2" alt="..."></img></a>
+        <div className='ttl-2'>What did each company contribute to the new project?</div>
+
+        <p className='Sub-main mt-3' href='#Hello'>Bancomer desarrolló una aplicación única para la plataforma Oil Profit México. Una de las compañías más grandes del país, Pemex, y el gigante financiero Grupo Televisa, garantizaron la no imposición de impuestos sobre las ganancias de la plataforma. Este es el logro más impresionante de la industria financiera en la última década. Pemex proporcionó su base y recursos para que la plataforma funcione, permitiendo a inversores y fundadores ganar dinero.<br /><br />
+          Carlos Slim anunció en las redes sociales el lanzamiento de la nueva plataforma para finalmente presentarla al público en general.<i><a href='#Hello'> He claims that he will be able to change the situation in the country and allow each Mexican to passively earn up to 300,000 Mexican pesos per month with the country's natural resources.</a></i></p>
+        <a href='#Hello'><img src="assets/images/mexico_img-2.png" class="img-fluid my-3 mb-2" alt="..."></img></a>
+        <div className='ttl-2'>Why is the platform only available for people from Mexico?</div>
+        <p className='Sub-main mt-3'>Este hecho ha decepcionado claramente a las personas de otros países; comenzaron a escribir activamente a Carlos Slim en las redes sociales y recibieron una respuesta de él.<br /><br />
+          Carlos Slim anunció que habría un número limitado de plazas disponibles para todos los participantes para garantizar un ingreso alto y estable, y se decidió crear esta oportunidad de inversión exclusivamente para personas de México. Debe recordarse que Oil Profit México fue fundada en México, y todo esto es gracias a las personas talentosas de nuestro país.<br /><br />
+          En la segunda etapa del lanzamiento de la plataforma, habrá 125,000 plazas disponibles para nuevos participantes. La primera fase de prueba de la plataforma, que duró desde el 11 de diciembre de 2022 hasta el 10 de enero del año en curso y contó con la participación de 5,000 personas, fue exitosa, y ahora Oil Profit México está lista para abrir el acceso a todos los interesados.
+        </p>
+       <a href='#Hello'> <img src="assets/images/mexico_img-3.png" class="img-fluid my-3 mb-2" alt="..."></img></a>
+        <a href='#Hello'><strong><em>Según los datos oficiales de los desarrolladores, podemos concluir que este tipo de ingresos es completamente pasivo y no requiere participación activa del depositante.</em></strong></a>
+
+        <h3 className='my-3'>Beneficios del desarrollo único de Oil Profit México:</h3>
+        <div className="features">
+
+          <li>
+            <span className="feature-number">1. Accesibilidad para todos: </span>
+            Cada ciudadano mexicano puede convertirse en miembro de la plataforma. Solo necesita completar un registro sencillo y recibir la confirmación de un especialista después de su llamada.
+          </li>
+          <li>
+            <span className="feature-number">2. Cero por ciento de impuestos: </span>
+            Todos los ingresos están disponibles para el inversor y no están sujetos a impuestos. Otros inversores pagan alrededor del 25 por ciento sobre todos los ingresos de dividendos, ventas de acciones o ventas de criptomonedas.
+          </li>
+          <li>
+            <span className="feature-number">3. Simplicidad: </span>
+            La plataforma Oil Profit México es simple e intuitiva; incluso sin ningún conocimiento de operaciones con criptomonedas o acciones, puede comenzar a ganar en menos de 10 minutos de trabajo en la plataforma.
+          </li>
+          <li>
+            <span className="feature-number">4. Comodidad: </span>
+            El algoritmo hará todo por usted; no tendrá que ayudarlo ni realizar ninguna acción en lugar de él. La plataforma de inversión Oil Profit México ganará por usted.
+          </li>
+          <li>
+            <span className="feature-number">5.   Sin riesgo: </span>
+            El 99 por ciento de las operaciones terminan a su favor, lo que significa que obtendrá ganancias en casi todas las operaciones.
+          </li>
+          <li>
+            <span className="feature-number">6. Fiabilidad:</span>
+            La plataforma es pública y respaldada por el gobierno, por lo que no hay riesgo para ninguno de los participantes.
+          </li>
+
+        </div>
+        <a href='#Hello'><img src="assets/images/mexico_img-4.png" class="img-fluid my-3 mb-2" alt="..."></img></a>
+
+        <p className='Sub-main mt-3'>Las sorpresas de Oil Profit México no terminan ahí. Carlos Slim hizo una adición importante con respecto al 0% de impuestos sobre las ganancias de la plataforma.<i><a href='#Hello'>No pagas ni un centavo al gobierno porque todo el dinero se convierte en criptomonedas, que se sabe que no están sujetas a impuestos.</a></i> Ni siquiera necesitas una billetera de criptomonedas; recibes la cantidad completa en tu tarjeta bancaria, ¡y es <i><a href='#Hello'>completamente oficial!</a></i><br />
+          <p className='Sub-main mt-3'>Los desarrolladores de la plataforma también se han ocupado de los ciudadanos de bajos ingresos al reducir el depósito mínimo a solo 4,500 pesos mexicanos, ¡dándole a todos la oportunidad de convertirse en microinversores! Esto se debe a que muchas familias mexicanas gastan una cantidad similar en cosas innecesarias cada semana, y si pasas una semana sin ellas, puedes multiplicar este monto en el primer mes.</p>
+        </p>
+        <h2 className='left'>Para convertirse en miembro de Oil Profit México, solo necesita seguir algunos pasos simples:</h2>
+        <li>
+          <span className="feature-number">1.</span>
+          Regístrese y complete todos los campos.
+        </li>
+        <li>
+          <span className="feature-number">2.</span>
+          Espere una llamada de un asesor financiero y confirme su registro.
+        </li>
+        <li>
+          <span className="feature-number">3.</span>
+          Elija la cantidad de inversión deseada y comience a ganar el mismo día.
+        </li>
+        <div className='fome' id='Hello'>
+            <img src="assets/images/imagess.png" class="img-fluid my-3 mb-2" alt="..."></img>
+            <div className='myfome'>
+              <div className="form-container">
+        <form className="outside">
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" />
+          </div>
+          <div>
+            <label htmlFor="number">Number:</label>
+            <input type="text" id="number" name="number" />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <button className='submit' type="submit">Submit</button>
+        </form>
+      </div>
+
+            </div>
+        </div>
+      
+
+<div className='Comentarios mt-3' href="#Hello">
+  <h4>Comentarios</h4>
+</div>
+<a href='#Hello'><img src="assets/images/comments.png" class="img-fluid my-3 mb-2" alt="..."></img></a>
+</div>
+    </>
+  );
+}
+
+export default App;
